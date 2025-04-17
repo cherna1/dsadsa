e508bf28bbfaf9898d60426f23dca64cb64bdbc5
cherna1 <banana-09@inbox.ru>
2025-04-17 23:22:10 +0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Winnable-Hub/Loader/refs/heads/main/Loader.Lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YOies/KazounVN/refs/heads/main/ARX"))()
